2182fe6ece092e25e5252b4286a17a195fefe502
Avishka <avishkakapuruge@gmail.com>
2024-01-21 18:57:18 +0530

    camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useRef } from 'react';
-import { Mesh, Shape, DoubleSide } from 'three';
-
-const getHeartShape = () => {
-  const x = -5, y = -10;
-
-  const heartShape = new Shape();
-
-  heartShape.moveTo(x + 5, y + 5);
-  heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
-  heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
-  heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
-  heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
-  heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
-  heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
-
-  return heartShape;
-};
+import { Mesh, DoubleSide, PerspectiveCamera } from 'three';
 
 const Box = () => {
 
@@ -30,9 +14,9 @@ const Box = () => {
   });
 
   return (
-    <mesh ref={boxRef} scale={[.1, .1, .1]}>
-      <shapeGeometry args={[getHeartShape()]} />
-      <meshStandardMaterial color='pink' side={DoubleSide} />
+    <mesh ref={boxRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color='orange' side={DoubleSide} />
     </mesh>
   );
 };
@@ -48,15 +32,21 @@ const Controls = () => {
   );
 };
 
+function CameraHelper() {
+  const camera = new PerspectiveCamera(60, 1, 1, 3);
+  return <group position={[0, 0, 2]}><cameraHelper args={[camera]} />
+  </group>
+}
+
 const ThreeScene = () => {
   return (
-    <Canvas>
+    <Canvas camera={{position: [0, 0, 2], fov: 60, near: 1, far: 3}}>
       <ambientLight />
       <pointLight position={[5, 5, 5]} intensity={3} />
       <pointLight position={[-3, -3, 2]} />
-      <axesHelper args={[10]} />
       <Controls />
       <Box />
+      <CameraHelper />
     </Canvas>
   );
 };
