201eeedc64202299641cf18742e3003fbb60fc33
Avishka <avishkakapuruge@gmail.com>
2024-01-21 19:19:02 +0530

    3d model

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { useRef } from 'react';
-import { Mesh, DoubleSide, PerspectiveCamera } from 'three';
-
-const Box = () => {
-
-  const boxRef = useRef<Mesh>(null);
-
-  useFrame(() => {
-    if (boxRef.current) {
-      boxRef.current.rotation.x += 0.005;
-      boxRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={boxRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color='orange' side={DoubleSide} />
-    </mesh>
-  );
-};
+import { ContactShadows, Float } from '@react-three/drei';
+import { Canvas, useThree } from '@react-three/fiber';
+import { Boombox } from './Boombox';
 
 const Controls = () => {
   const {
@@ -32,21 +13,17 @@ const Controls = () => {
   );
 };
 
-function CameraHelper() {
-  const camera = new PerspectiveCamera(60, 1, 1, 3);
-  return <group position={[0, 0, 2]}><cameraHelper args={[camera]} />
-  </group>
-}
-
 const ThreeScene = () => {
   return (
-    <Canvas camera={{position: [0, 0, 2], fov: 60, near: 1, far: 3}}>
+    <Canvas>
       <ambientLight />
-      <pointLight position={[5, 5, 5]} intensity={3} />
-      <pointLight position={[-3, -3, 2]} />
+      <pointLight position={[1, 1, 1]} intensity={5} />
+      <pointLight position={[-2, -2, 3]} />
       <Controls />
-      <Box />
-      <CameraHelper />
+      <Float speed={1.4} rotationIntensity={1.5} floatIntensity={2.3}>
+        <Boombox />
+      </Float>
+      <ContactShadows position={[0, -0.3, 0]} blur={2.5} scale={20} far={10} />
     </Canvas>
   );
 };
