507c499c93b648a04abf8f5b74e7752e542a6d25
Avishka <avishkakapuruge@gmail.com>
2024-03-03 17:40:33 +0530

    textures

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,46 @@
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, Plane, useHelper, useTexture } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import { useRef } from 'react';
-import { Mesh } from 'three';
+import { PointLight, PointLightHelper } from 'three';
 
-const Box = () => {
 
-  const boxRef = useRef<Mesh>(null);
+const Terrain = () => {
+  const terrainTexture = useTexture({
+    map: "/textures/aerial_rocks_02_diff_4k.jpg",
+    displacementMap: "/textures/aerial_rocks_02_disp_4k_edited.jpg",
+    aoMap: "/textures/aerial_rocks_02_arm_4k_ao_edited.jpg",
+    roughnessMap: "/textures/aerial_rocks_02_arm_4k_roughness_edited.jpg",
+    metalnessMap: "/textures/aerial_rocks_02_arm_4k_metalness_edited.jpg",
+    normalMap: "/textures/aerial_rocks_02_nor_gl_4k_edited.jpg",
+    alphaMap: "/textures/alpha.png",
 
-  useFrame(() => {
-    if (boxRef.current) {
-      boxRef.current.rotation.x += 0.005;
-      boxRef.current.rotation.y += 0.01;
-    }
   });
-
   return (
-    <mesh ref={boxRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color='pink' />
-    </mesh>
+    <Plane args={[10, 10, 128, 128]} rotation-x={-Math.PI / 2}>
+      <meshStandardMaterial {...terrainTexture} transparent />
+    </Plane>
   );
 };
 
-const Controls = () => {
-  const {
-    camera,
-    gl: { domElement},
-  } = useThree();
+const ThreeContent = () => {
+  const lightRef = useRef<PointLight>(null);
+  useHelper(lightRef, PointLightHelper, 1, "red");
 
   return (
-    <orbitControls args={[camera, domElement]} />
-  )
-}
+    <>
+      <ambientLight />
+      <pointLight ref={lightRef} position={[5, 5, 0]} intensity={4} />
+      <OrbitControls />
+      <Terrain />
+    </>
+  );
+};
 
 const ThreeScene = () => {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[5, 5, 5]} />
-      <axesHelper args={[10]}/>
-      <Controls />
-      <Box />
+    <Canvas camera={{ position: [0, 10, 5] }}>
+      <ThreeContent />
+
     </Canvas>
   );
 };
